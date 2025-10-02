78e1a12e158fddd6ea11e5f0b72df69a5b571cec
Mehul <yourdevmehul@gmail.com>
2025-10-02 14:26:53 +0530

    Optimised Bubble Sort

--- Sorting/bubble.cpp
+++ Sorting/bubble.cpp
@@ -4,10 +4,15 @@ using namespace std;
 vector<int> bubbleSort(vector<int>& nums){
     int n = nums.size();
     for(int i=0;i<=n-1;i++){
-        for(int j=0; j<n-1;j++){
+        int swapDone = 0;
+        for(int j=0; j<n-i;j++){
             if(nums[j]>nums[j+1]){
                 swap(nums[j],nums[j+1]);
+                swapDone = 1
             }
+        }
+        if(swapDone==0){
+            break;
         }
     }
     return nums;
@@ -33,7 +38,7 @@ int main(){
     cout<< endl;
 
     nums=bubbleSort(nums);
-    
+
     cout<<"Array after Bubble Sort"<<endl;
     for(int i=0;i<n;i++){
         cout<<nums[i]<<" ";
