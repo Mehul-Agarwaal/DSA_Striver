69fd1eea84f0b331c826f1d96ecf29add0a650df
Mehul <yourdevmehul@gmail.com>
2025-10-02 14:11:13 +0530

    Bubble Sort

---
+++ Sorting/bubble.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> bubbleSort(vector<int>& nums){
+    int n = nums.size();
+    for(int i=0;i<=n-1;i++){
+        for(int j=0; j<n-1;j++){
+            if(nums[j]>nums[j+1]){
+                swap(nums[j],nums[j+1]);
+            }
+        }
+    }
+    return nums;
+}
+int main(){
+    vector<int> nums;
+    int n;
+
+    cout<<"Enter Number of elements in the Array:"<<endl;
+    cin>>n;
+       
+    nums.resize(n);
+
+    cout<<"Enter the numbers one by one:"<<endl;
+    for(int i=0 ; i<n ; i++){
+        cin>>nums[i];
+    }
+
+    cout<<"Here is the array you entered:"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<nums[i]<<" ";
+    }
+    cout<< endl;
+
+    nums=bubbleSort(nums);
+    
+    cout<<"Array after Bubble Sort"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<nums[i]<<" ";
+    }
+
+}
